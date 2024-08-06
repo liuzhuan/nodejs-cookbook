7e7749f898f6bb6b363a4710b2a713476b584553
liuzhuan <liuz_geek@163.com>
2024-08-06 16:34:56 +0800

    feat(http): get post data

--- 01_http-server/index.mjs
+++ 01_http-server/index.mjs
@@ -7,9 +7,23 @@ const server = createServer((req, res) => {
   req.query = querystring.parse(req.url.split('?')[1])
   console.log(`query string: ${JSON.stringify(req.query)}`)
 
-  // use `document.charset` to see current document's charset
-  res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
-  res.end(`Hello Node.js!\nquery string: ${JSON.stringify(req.query)}`)
+  if (req.method === 'POST') {
+    console.log('content-type:', req.headers['content-type'])
+
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+
+    req.on('end', () => {
+      console.log(`post data: ${postData}`)
+      res.end('hello world')
+    })
+  } else {
+    // use `document.charset` to see current document's charset
+    res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
+    res.end(`Hello Node.js!\nquery string: ${JSON.stringify(req.query)}`)
+  }
 })
 
 server.listen(3000, '127.0.0.1', () => {
