e3f6fec6c778845f27b764c36c7c105dd286fca1
liuzhuan <liuz_geek@163.com>
2024-08-06 16:12:29 +0800

    feat(http): get query string from url

--- 01_http-server/index.mjs
+++ 01_http-server/index.mjs
@@ -1,9 +1,14 @@
 import { createServer } from 'node:http'
+import querystring from 'node:querystring'
 
 const server = createServer((req, res) => {
   console.log(req.method, req.url)
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World!\n')
+  const qs = querystring.parse(req.url.split('?')[1])
+  console.log(`query string: ${JSON.stringify(qs)}`)
+
+  // use `document.charset` to see current document's charset
+  res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
+  res.end(`Hello Node.js!\nquery string: ${JSON.stringify(qs)}`)
 })
 
 server.listen(3000, '127.0.0.1', () => {
